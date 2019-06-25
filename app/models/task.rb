5c832247ca6dc8e145819f491cf54d65149c933b
yuya.ishii <yypswm61@gmail.com>
2019-06-25 13:57:17 +0000

    add_column_kadai

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-    
+    validates  :status, presence:true, length:{maximum:10}
+    validates  :content, presence:true, length:{maximum:10}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_162249) do
+ActiveRecord::Schema.define(version: 2019_06_24_084458) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
